2a40f5656459e74b28b9df954330f4edaaecc5f4
Robert Alexandru <romaniarobertverde@yahoo.com>
2019-08-21 00:27:57 +0100

    Declarin a variable and then we assign a value to it!

--- src/script.js
+++ src/script.js
@@ -1,6 +1,15 @@
-// console.dir(document) is helping you to see detailed document in the browser console.
-console.dir(document);
+
+console.dir(document); //is helping you to see detailed document in the browser console.
 console.log('hey');
 // document.querySelector('h1').innerHTML = "Hello everyone"; Is leting you to update any html element
-document.querySelector('h1').innerHTML = "Hello everyone";
+
+var message = "Hello World"; // Declaring a variable "message", and then we are assign a value(string) to message
+document.querySelector('h1').innerHTML = message; // Is leting you to update any html element
 document.querySelector('button').innerHTML = "Click me";
+
+
+
+var a = "World";
+var b = "Hello";
+var c = b + ' ' + a;
+console.log (c)
