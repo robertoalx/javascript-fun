042f63aef1960b24e8f03a2a34c1af0d552da463
Robert Alexandru <romaniarobertverde@yahoo.com>
2019-08-20 16:46:04 +0100

    Updating html elements with javaScript

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-alert('haihui')
+// console.dir(document) is helping you to see detailed document in the browser console.
+console.dir(document);
+console.log('hey');
+// document.querySelector('h1').innerHTML = "Hello everyone"; Is leting you to update any html element
+document.querySelector('h1').innerHTML = "Hello everyone";
+document.querySelector('button').innerHTML = "Click me";
