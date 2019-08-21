3e3588617ab2348d808ac66c0c070393c83ba3bc
Robert Alexandru <romaniarobertverde@yahoo.com>
2019-08-21 14:46:05 +0100

    Second Attempt- Use functions to increment with 1 each time when you Click a button.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+/*                    first attempt
 var b = 0;
 function message(a) {
   b++;
@@ -20,7 +21,32 @@ function message2(theLastOne) {
   var output2 = theLastOne + ' ' + third;
   document.querySelector('h1').innerHTML = output2;
   console.log(theLastOne)
+}*/
+
+//                 second attempt where only the numbers will apear when we click the button.
+
+var var1, var2, var3;
+var1 = var2 = var3 = 0;
+
+function message1() {
+  var1++;
+  message()
 }
+
+function message2() {
+  var2++;
+  message()
+}
+
+function message3() {
+  var3++;
+  message();
+}
+
+function message() {
+  document.querySelector('h1').innerHTML = var1 + '' + var2 + '' +var3;
+}
+
 
 // console.dir(document); //is helping you to see detailed document in the browser console.
 // console.log('hey');
