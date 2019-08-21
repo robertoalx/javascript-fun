c94d4fcf8fb00c048e550d10696d74d1064f0b84
Robert Alexandru <romaniarobertverde@yahoo.com>
2019-08-21 18:28:37 +0100

    Playing a bit with array and use powerfull array methods: push, pop, shift and unshift

--- src/script.js
+++ src/script.js
@@ -54,9 +54,9 @@ function plus(a,b) {
   return a + b;
 } */
 
-//             Objects
+//                     Objects
 
-var computer = {
+/*var computer = {
   motherboard: 'Gigabyte',
   procesor: 'Intel core I7 8600',
   memory: '16gb 2300mhz',
@@ -66,7 +66,17 @@ var computer = {
   keybord: 'Razer',
   mouse: 'Razer Deathadder'
 }
-document.querySelector('h1').innerHTML = computer.motherboard + ' is having an amazing ' + computer.procesor + ' on top of that the graphic card ' + computer.graphic + ' is doing the job amazingly';
+document.querySelector('h1').innerHTML = computer.motherboard + ' is having an amazing ' + computer.procesor + ' on top of that the graphic card ' + computer.graphic + ' is doing the job amazingly';*/
+
+
+//                    Arrays
+var myArray = ['Robert', 'Alexandru', 29, true];
+var arr1 = myArray.push('This string is pushed by me at the end of an array!')
+var arr2 = myArray.pop();//pop() removes the last index.
+var arr3 = myArray.shift();//shift() will delete the firxt index, that's mean the 0 index
+var arr4 = myArray.unshift("Gabriel"); //shift() adding one ore more elements to the beginning of an array and return a new length of the array 
+console.log(myArray);
+
 
 // console.dir(document); //is helping you to see detailed document in the browser console.
 // console.log('hey');
