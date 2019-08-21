cd5d22fd100f5975240d304f23bee1fd6994dd01
Robert Alexandru <romaniarobertverde@yahoo.com>
2019-08-21 19:25:22 +0100

    Using conditional statement if else is amazing and logical.

--- src/script.js
+++ src/script.js
@@ -70,12 +70,24 @@ document.querySelector('h1').innerHTML = computer.motherboard + ' is having an a
 
 
 //                    Arrays
-var myArray = ['Robert', 'Alexandru', 29, true];
+/*var myArray = ['Robert', 'Alexandru', 29, true];
 var arr1 = myArray.push('This string is pushed by me at the end of an array!')
 var arr2 = myArray.pop();//pop() removes the last index.
 var arr3 = myArray.shift();//shift() will delete the firxt index, that's mean the 0 index
 var arr4 = myArray.unshift("Gabriel"); //shift() adding one ore more elements to the beginning of an array and return a new length of the array 
-console.log(myArray);
+console.log(myArray);*/
+
+//                    Condition statement
+
+var num = 15;
+
+if (num !== 14 && num > 20) {
+  console.log('You know that your true')
+} else if (num === 15 || num <20) {
+  console.log('It is equal') 
+} else {
+  console.log('Go to sleep')
+}
 
 
 // console.dir(document); //is helping you to see detailed document in the browser console.
