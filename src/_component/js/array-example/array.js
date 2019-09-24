c931bc500a23b1d3c7a2130d594ed7838b1aaed9
Robert Alexandru <romaniarobertverde@yahoo.com>
2019-09-24 17:44:40 +0100

    array exapmple with push,pop, shift and unshift methods.

---
+++ src/_component/js/array-example/array.js
@@ -0,0 +1,6 @@
+var myArray = ['Robert', 'Alexandru', 29, true];
+var arr1 = myArray.push('This string is pushed by me at the end of an array!')
+var arr2 = myArray.pop();//pop() removes the last index.
+var arr3 = myArray.shift();//shift() will delete the firxt index, that's mean the 0 index
+var arr4 = myArray.unshift("Gabriel"); //shift() adding one ore more elements to the beginning of an array and return a new length of the array 
+console.log(myArray);
--- src/script.js
+++ src/script.js
@@ -68,15 +68,6 @@ function plus(a,b) {
 }
 document.querySelector('h1').innerHTML = computer.motherboard + ' is having an amazing ' + computer.procesor + ' on top of that the graphic card ' + computer.graphic + ' is doing the job amazingly';*/
 
-
-//                    Arrays
-/*var myArray = ['Robert', 'Alexandru', 29, true];
-var arr1 = myArray.push('This string is pushed by me at the end of an array!')
-var arr2 = myArray.pop();//pop() removes the last index.
-var arr3 = myArray.shift();//shift() will delete the firxt index, that's mean the 0 index
-var arr4 = myArray.unshift("Gabriel"); //shift() adding one ore more elements to the beginning of an array and return a new length of the array 
-console.log(myArray);*/
-
 //                    Condition statement
 
 /*var num = 15;
